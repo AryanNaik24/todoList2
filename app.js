1a8e0ba44078e92658307812dca5d18f438b2bc0
AryanNaik <aryannaikofficial@gmail.com>
2023-07-13 00:22:51 +0530

    complete

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose=require("mongoose");
-
+const _=require("lodash");
 
 const app = express();
 
@@ -72,7 +72,7 @@ app.get("/", function(req, res) {
 });
  
 app.get ("/:customListName",(req,res)=>{
-  const customListName=req.params.customListName;
+  const customListName=_.capitalize(req.params.customListName);
   List.findOne({name:customListName}).then((foundList)=>{
     if (!foundList){
       const list = new List({
